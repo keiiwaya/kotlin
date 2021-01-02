31b58b69efcfe1af95032af8100b1a9b321358ef
kei <qk1007keip@icloud.com>
2021-01-02 21:43:45 +0900

    2021/01/02

--- ChangeColorApp/app/src/main/java/com/example/changecolorapp/MainActivity.kt
+++ ChangeColorApp/app/src/main/java/com/example/changecolorapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.changecolorapp
 
 import android.graphics.Color
+import android.graphics.ColorSpace
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +18,33 @@ class MainActivity : AppCompatActivity() {
         val button2 = findViewById<Button>(R.id.button2)
         val constraint = findViewById<ConstraintLayout>(R.id.constraint)
 
+        val rTextView = findViewById<TextView>(R.id.rView)
+        val gTextView = findViewById<TextView>(R.id.gView)
+        val bTextView = findViewById<TextView>(R.id.bView)
+
+
         button.setOnClickListener {
-            constraint.setBackgroundColor(Color.RED)
+            val r = ransuu()
+            val g = ransuu()
+            val b = ransuu()
+
+            constraint.setBackgroundColor(Color.rgb(r, g, b))
+
+            rTextView.text = r.toString()
+            gTextView.setText(r.toString())
+            bTextView.setText(b.toString())
+
         }
 
         button2.setOnClickListener {
             constraint.setBackgroundColor(Color.CYAN)
         }
 
+    }
+
+    fun ransuu(): Int {
+        val x = (0..256).random()
+
+        return x
     }
 }
